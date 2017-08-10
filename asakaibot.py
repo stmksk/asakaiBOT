16792c0376704cbedf56e9a359589bcb7767d895
stmksk <ksaotome@crimson.ua.edu>
2017-08-10 15:57:02 +0900

    first commit

---
+++ asakaibot.py
@@ -0,0 +1,9 @@
+from slacker import Slacker
+import json
+slacktoken = open("slack.json").read()
+token =  json.loads(slacktoken)
+
+slack = Slacker(token['token'])
+
+# Send a message to #general channel
+slack.chat.post_message('#test', 'Hello fellow slackers!')
