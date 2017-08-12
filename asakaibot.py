6981a7afdcd287799d44788d1960d58876afcb2b
ksaotome <kousuke.saotome@liferobotics.jp>
2017-08-13 01:10:42 +0900

    jsonのメンバーをシャッフルして流せるようにした

--- asakaibot.py
+++ asakaibot.py
@@ -1,9 +1,18 @@
 from slacker import Slacker
 import json
-slacktoken = open("slack.json").read()
-token =  json.loads(slacktoken)
+import random
 
-slack = Slacker(token['token'])
+slackjson = open("slack.json").read()
+settings =  json.loads(slackjson)
 
-# Send a message to #general channel
-slack.chat.post_message('#test', 'Hello fellow slackers!', as_user=True)
+slack = Slacker(settings['token'])
+members = settings['members']
+random.shuffle(members)
+
+# Send a message to #test channel
+slack.chat.post_message(
+        type="message",
+        channel='#test',
+        text="今日の朝会は["+" ▶ ".join(members)+"]の順です。\n"+
+        "司会は["+members[0]+"]さんお願いします。",
+        as_user=True)
