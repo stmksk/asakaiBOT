2c2efb6754d6e8df9bcbeab306d2d212a5e8518a
stmksk <ksaotome@crimson.ua.edu>
2017-08-17 00:38:05 +0900

    シャッフルが正しく動くようにした、@userのリンクが正しくリンクされるようにした ..etc

--- plugins/asakaibot.py
+++ plugins/asakaibot.py
@@ -1,15 +1,11 @@
 # coding: utf-8
 
 import json
-import random
 import re
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 from slackbot.bot import default_reply
 
-slackjson = open("slack.json").read()
-settings =  json.loads(slackjson)
-members_old = settings['members']
 
 @respond_to('[sprint|Sprint][0-9]*$')
 def burndown(message):
@@ -18,14 +14,15 @@ def burndown(message):
     sprintNum = int(re.search(pattern, textbody).group()) + 94
     message.send("https://dev.liferobotics.jp/redmine/rb/burndown/" + str(sprintNum))
 
+"""
 @respond_to('朝会ﾀﾞﾖ')
 def asakai_order_func(message):
     members = settings['members_mention']
     shuffled = list(members.keys()); random.shuffle(list(members.keys()))
     message.send("今日の朝会は\n"+" ▶ ".join(shuffled)+"の順です。\n司会は["+shuffled[0]+" "+members[shuffled[0]]+"]さんお願いします。")
+"""
 
 @listen_to('ｱｻｶｲ')
 def asakai_test(message):
     message.send("ｱｻｶｲ？")
-
 
